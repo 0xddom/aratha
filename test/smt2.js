e30d3c447ad6b137b7de130d203ac6cf985e48fc
Daniel Dominguez <daniel@veridise.com>
2024-05-22 20:00:36 +0200

    Add z3str test

--- test/smt2.js
+++ test/smt2.js
@@ -24,3 +24,14 @@ describe("running Z3", function() {
     });
   });
 });
+
+describe("running Z3Str", function() {
+  context("on common.smt2", function() {
+    const z3Path = testZ3();
+    it("should terminate with no errors", function(done) {
+      const preludePath = path.resolve(__dirname, "../lib/smt2/z3str/prelude.smt2");
+      const filePath = path.resolve(__dirname, "../lib/smt2/common.smt2");
+      child_process.execFile(z3Path, ["-smt2", "smt.string_solver=z3str3", preludePath, filePath], done);
+    });
+  });
+});
