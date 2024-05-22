d7320bd417e37d5319a17fae326786e5fabde1fc
Daniel Dominguez <daniel@veridise.com>
2024-05-22 19:48:08 +0200

    Fix z3 test

--- test/smt2.js
+++ test/smt2.js
@@ -5,21 +5,22 @@ const path = require("path");
 const { testCVC4, testZ3 } = require("./testUtils");
 
 describe("running CVC4", function() {
-    context("on common.smt2", function() {
-        const cvc4Path = testCVC4();
-        it("should terminate with no errors", function(done) {
-            const filePath = path.resolve(__dirname, "../lib/smt2/common.smt2");
-            child_process.execFile(cvc4Path, ["--lang=smt2", "--strings-exp", "--incremental", filePath], done);
-        });
+  context("on common.smt2", function() {
+    const cvc4Path = testCVC4();
+    it("should terminate with no errors", function(done) {
+      const filePath = path.resolve(__dirname, "../lib/smt2/common.smt2");
+      child_process.execFile(cvc4Path, ["--lang=smt2", "--strings-exp", "--incremental", filePath], done);
     });
+  });
 });
 
 describe("running Z3", function() {
-    context("on common.smt2", function() {
-        const z3Path = testZ3();
-        it("should terminate with no errors", function(done) {
-            const filePath = path.resolve(__dirname, "../lib/smt2/common.smt2");
-            child_process.execFile(z3Path, [filePath], done);
-        });
+  context("on common.smt2", function() {
+    const z3Path = testZ3();
+    it("should terminate with no errors", function(done) {
+      const preludePath = path.resolve(__dirname, "../lib/smt2/z3/prelude.smt2");
+      const filePath = path.resolve(__dirname, "../lib/smt2/common.smt2");
+      child_process.execFile(z3Path, ["-smt2", preludePath, filePath], done);
     });
+  });
 });
